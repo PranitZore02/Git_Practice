4c3b26b648503debd54355541717e9ba90bb7bf0
Pranit Zore” <pranitzore65@gmail.com>
2023-07-22 10:36:00 +0530

    added code

---
+++ App.py
@@ -0,0 +1,11 @@
+def sum_of_no(lst):
+    total=0
+    for i in range(len(lst)):
+        total=total+lst[i]
+    return total
+
+
+
+lst=[1,2,3,4,5,6]
+result=sum_of_no(lst)
+print(f'the sum of the given no is {result}')
